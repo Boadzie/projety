21f5d0b95fbc2bd39ff84d9b7dec2ffbc279f980
Daniel Boadzie <boadziedaniel43@gmail.com>
2022-08-08 17:35:04 +0000

    Add Tailwindcss to app
    Add Navbar and Banner with Tailwindcss styling

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :projety, ProjetyWeb.Endpoint,
   secret_key_base: "2akhre8iRKjV+G+mOEaW7DZ6nzJ64ImutSXHJkko07VIPMOJjgON6DMzIV/zle3X",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
